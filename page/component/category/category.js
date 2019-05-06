a7d83a32fe5a2dfc7b32843edb193fe91f18fa72
bowenxiaoya <2317446934@qq.com>
2019-05-06 17:02:40 +0800

    txt

---
+++ page/component/category/category.js
@@ -0,0 +1,49 @@
+Page({
+    data: {
+        category: [
+            {name:'果味',id:'guowei'},
+            {name:'蔬菜',id:'shucai'},
+            {name:'炒货',id:'chaohuo'},
+            {name:'点心',id:'dianxin'},
+            {name:'粗茶',id:'cucha'},
+            {name:'淡饭',id:'danfan'}
+        ],
+        detail:[],
+        curIndex: 0,
+        isScroll: false,
+        toView: 'guowei'
+    },
+    onReady(){
+        var self = this;
+        wx.request({
+          url:'https://www.luways.com/web/unLogin/txtInfo',
+            success(res){
+              console.log(res)
+                self.setData({
+                    
+                    detail : res.data
+                })
+            }
+        });
+        
+    },
+    switchTab(e){
+      const self = this;
+      this.setData({
+        isScroll: true
+      })
+      setTimeout(function(){
+        self.setData({
+          toView: e.target.dataset.id,
+          curIndex: e.target.dataset.index
+        })
+      },0)
+      setTimeout(function () {
+        self.setData({
+          isScroll: false
+        })
+      },1)
+        
+    }
+    
+})
